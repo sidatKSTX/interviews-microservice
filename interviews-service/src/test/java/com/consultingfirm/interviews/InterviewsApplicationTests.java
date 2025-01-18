2687e096c22249d4bba8aa2e7f9668f8f4198cea
bsmahi <bs.mahi@gmail.com>
2025-01-18 12:29:26 +0530

    Added Interviews Service

---
+++ interviews-service/src/test/java/com/consultingfirm/interviews/InterviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultingfirm.interviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
